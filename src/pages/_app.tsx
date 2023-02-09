b785de6b7f13493799a5bf7669fb8557ac5ba5e4
Charlie Bellow <charliebftm@gmail.com>
2023-02-09 16:53:10 -0300

    project working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
 import { SidebarProvider } from '../components/Sidebar/SidebarContext';
-
-
 import Sidebar from '../components/Sidebar/Sidebar';
+
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,14 @@
 
-import { SidebarProvider } from '../components/Sidebar/SidebarContext';
 import Home from '../pages/Home';
 
-import Sidebar from '../components/Sidebar/Sidebar';
 
 function App () {
   return (
-    <SidebarProvider>
-      
-        <Sidebar>
+
           
              <Home /> 
            
-        </Sidebar>
-      
-    </SidebarProvider>
+ 
   );
 }
 
