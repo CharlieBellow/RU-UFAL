4456d62153e6105d4e05270be64ea39c5d7ecde9
Charlie Bellow <charliebftm@gmail.com>
2023-02-09 09:32:47 -0300

    migrating project to next

--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+import Link from "next/link";
 import { useState, useContext, ReactNode } from "react"
 import SidebarContext from "./SidebarContext";
 import * as Icon from "phosphor-react";
@@ -18,7 +18,7 @@ function SidebarItem(props: Item){
     }
     return (
         <div onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-            <Link to={props.item.path? props.item.path : "#"} className={`flex flex-col w-full justify-center`} onClick={() => selectItem()} >
+            <Link href={props.item.path? props.item.path : "#"} className={`flex flex-col w-full justify-center`} onClick={() => selectItem()} >
                 <div  className={`flex flex-row justify-between items-center text-white mx-2 my-2 py-2 gap-4 rounded-lg ease-in-out duration-300 hover:bg-blue-700 cursor-pointer ${selected && open && !props.item.path ? "bg-blue-700" : "bg-blue-600"}`}>
                    <div className="flex flex-row items-center gap-2">
                         <div className="pl-2">
@@ -35,13 +35,13 @@ function SidebarItem(props: Item){
             <div className="flex flex-col w-full relative">                
                     {open && selected && props.item.children ? props.item.children.map((child : Item, index : number) => {
                         return <div className={`text-white flex flex-col mx-2 px-6 py-2 gap-4 rounded-lg items-start ease-in-out duration-300 hover:bg-blue-700 cursor-pointer`}>
-                            <Link to={child.dir? child.dir : "#"} key={index}>{child.title}</Link>
+                            <Link href={child.dir? child.dir : "#"} key={index}>{child.title}</Link>
                         </div>
                     }) : <></>} 
                     {!open && props.item.children ? 
                     <div className="bg-blue-600 text-white absolute left-14 -bottom-2 w-40 rounded-md z-10">
                         {props.item.children.map((child: Item, index : number) => {
-                            return <Link to={child.dir? child.dir : "#"} key={index} className={`hover:bg-blue-700 rounded-md p-2 ${hover ? "block" : "hidden"}`}>{child.title}</Link>
+                            return <Link href={child.dir? child.dir : "#"} key={index} className={`hover:bg-blue-700 rounded-md p-2 ${hover ? "block" : "hidden"}`}>{child.title}</Link>
                         })}
                     </div> 
                     : <></>}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.tsx"],
-	theme: {
-		extend: {
-			rotate:{
-				'12': '13deg',
-			},
-			boxShadow:{
-				'card': '0 6px 16px 0px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1);'
-			},
-			backgroundImage: {
-				gradient:
-					"linear-gradient(135deg, rgba(1, 149, 218, 0.67) 0%, #0195DA 16.29%, #1B4BA8 99.89%)",
-				"ufalBackground":
-					"url('/src/assets/ufal-sigla-branca-fundo-transparente-40por-cento.png')",
-			},
-		},
-	},
-	plugins: [],
-};
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
